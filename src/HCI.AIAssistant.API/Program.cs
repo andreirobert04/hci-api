497e76d4c5299def711f7cb43fa8c676ddb9dbd2
Barabas Robert <barabasrobert3006@gmail.com>
2025-11-28 14:11:21 +0200

    changes of API

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -5,6 +5,19 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
+
 // Replace appsettings.json values with Key Vault values
 var keyVaultName = builder.Configuration
     [$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"];
@@ -49,6 +62,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("CORS");
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
